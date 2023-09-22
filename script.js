207fdff9bea981ec3093fa2395bb04d98087ce5a
chieftainblazer <110900442+chieftainblazer@users.noreply.github.com>
2023-09-22 14:30:33 -0700

    Add the sequence to the display
    Managed to get the +, -, * and / operations work.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const INPUT_1_TURN = 1;
+const INPUT_0_TURN = 0;
+
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -18,7 +21,7 @@ function operate(number1, number2, operator) {
     if (operator === "/") {
         return divide(number1, number2);
     }
-    if (operator === "*") {
+    if (operator === "x") {
         return multiply(number1, number2);
     }
     if (operator === "-") {
@@ -44,8 +47,23 @@ function operate(number1, number2, operator) {
     }
 }
 
-let number1;
-let number2;
-let operator;
+function populateDisplay(event) {
+    const currentChoice = event.target.value;
+    if (currentChoice === "AC") {
+        currentExpression = "";
+        display.textContent = "|";
+    } else if (currentChoice === "=") {
+        display.textContent = Function("return " + currentExpression)();
+    } else {
+        currentExpression += currentChoice;
+        display.textContent = currentExpression;
+    }
+}
 
-console.log(operate(1, 2, "+"));
+let currentExpression = "";
+const display = document.querySelector('.display');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', populateDisplay)
+});
