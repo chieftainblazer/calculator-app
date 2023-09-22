bf8d0c53dfd3fd831269ebd0c352bac6786f3f0d
chieftainblazer <110900442+chieftainblazer@users.noreply.github.com>
2023-09-21 18:17:05 -0700

    Add the operate function
    Added the operate function to allow for 3 parameters to be
    given in order to call the necessary math function. Some blank
    code was added as well for future functionality.

--- script.js
+++ script.js
@@ -14,4 +14,38 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
-console.log(divide(1, 3));
+function operate(number1, number2, operator) {
+    if (operator === "/") {
+        return divide(number1, number2);
+    }
+    if (operator === "*") {
+        return multiply(number1, number2);
+    }
+    if (operator === "-") {
+        return subtract(number1, number2);
+    }
+    if (operator === "+") {
+        return add(number1, number2);
+    }
+    if (operator === "/") {
+        
+    }
+    if (operator === "/") {
+        
+    }
+    if (operator === "/") {
+        
+    }
+    if (operator === "/") {
+        
+    }
+    if (operator === "/") {
+        
+    }
+}
+
+let number1;
+let number2;
+let operator;
+
+console.log(operate(1, 2, "+"));
